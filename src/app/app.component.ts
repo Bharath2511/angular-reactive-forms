e7d6c5eab054dd2127c64ab75410180e31277a0f
Bharath2511 <bharathchandra630@gmail.com>
2022-06-24 15:42:19 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { CustomValidators } from "./custom-validators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  signupForm: FormGroup;
+  projectStatusList: { name: string; value: string }[] = [
+    { name: "Stable", value: "stable" },
+    { name: "Critical", value: "critical" },
+    { name: "Finished", value: "finished" },
+  ];
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      projectName: new FormControl(
+        null,
+        [Validators.required, CustomValidators.invalidProjectName],
+        CustomValidators.asyncInvalidProjectName
+      ),
+      email: new FormControl(null, [Validators.email, Validators.required]),
+      projectStatus: new FormControl("finished"),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
 }
